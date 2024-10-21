45bbfa45106097e5cdb1388d3335497c96c426e6
Avik Singha <102418811+EvilMorty13@users.noreply.github.com>
2024-10-21 17:13:53 +0600

    Request Body and POST requests | Pydantic pre-validators

--- awesome-project/main.py
+++ awesome-project/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI,HTTPException
-from schemas import GenreURLChoices,Band
+from schemas import GenreURLChoices,BandBase,BandCreate,BandWithID
 
 app = FastAPI()
 
@@ -15,11 +15,11 @@ BANDS = [
 
 
 @app.get('/bands')
-async def bands(genre:GenreURLChoices | None=None,has_albums:bool=False) -> list[Band]:
-    band_list = [Band(**b) for b in BANDS]
+async def bands(genre:GenreURLChoices | None=None,has_albums:bool=False) -> list[BandWithID]:
+    band_list = [BandWithID(**b) for b in BANDS]
     if genre:
         band_list = [
-           b for b in band_list if b.genre.lower() == genre.value
+           b for b in band_list if b.genre.value.lower() == genre.value
         ]
 
     if has_albums: 
@@ -29,8 +29,8 @@ async def bands(genre:GenreURLChoices | None=None,has_albums:bool=False) -> list
 
 
 @app.get('/bands/{band_id}')
-async def band(band_id:int) -> Band :
-    band = next((Band(**b) for b in BANDS if b['id']==band_id),None)
+async def band(band_id:int) -> BandWithID :
+    band = next((BandWithID(**b) for b in BANDS if b['id']==band_id),None)
     if band is None:
         raise HTTPException(status_code=404,detail='Band not found')
     return band
@@ -40,3 +40,13 @@ async def bands_for_genre(genre:GenreURLChoices) -> list[dict] :
     return[
         b for b in BANDS if b['genre'].lower() == genre.value
     ]
+
+
+
+@app.post("/bands")
+async def create_band(band_data: BandCreate) -> BandWithID:
+    id = BANDS[-1]['id'] + 1  # Corrected ID generation
+    band = BandWithID(id=id, **band_data.model_dump()).model_dump()
+    BANDS.append(band)
+    return band
+
--- awesome-project/schemas.py
+++ awesome-project/schemas.py
@@ -1,21 +1,33 @@
 from datetime import date
 from enum import Enum
-from pydantic import BaseModel
+from pydantic import BaseModel,field_validator
 
 class GenreURLChoices(Enum):
     ROCK = 'rock'
     POP = 'pop'
 
+class GenreChoices(Enum):
+    ROCK = 'Rock'
+    POP = 'Pop'
+
+
 class Album(BaseModel):
     title : str
     release_date : date
 
-class Band(BaseModel):
-    id : int
+class BandBase(BaseModel):
     name : str
-    genre : str
+    genre : GenreChoices
     albums: list[Album] = []
 
 
 
+class BandCreate(BandBase):
+    @field_validator('genre', mode='before')
+    def title_case_genre(cls, value):
+        if isinstance(value, str):
+            return value.title() 
+        raise ValueError('Genre must be a string')
 
+class BandWithID(BandBase):
+    id : int
